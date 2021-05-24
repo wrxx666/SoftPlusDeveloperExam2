6f031885a667b1e861f009872ea9cef458880e76
wrxx666 <70403604+wrxx666@users.noreply.github.com>
2021-05-24 16:22:49 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        int[] price = {5,100,20,66,16}; // Стартовые значения цен из примера
-        int[] checkResult = {0,0}; // Массив отображаемый в конце, для проверки работоспособности
+        int[] prices = {5,100,20,66,16}; // Стартовые значения цен из примера
+        int[] expectedResult = {0,0}; // Массив отображаемый в конце, для проверки работоспособности
         try {
-            checkResult = decryptData(price, 50, 1, 3);
+            expectedResult = decryptData(prices, 50, 1, 3);
         }
         catch (ArrayIndexOutOfBoundsException e){
             e.printStackTrace();
         }
-        for(int i : checkResult){
+        for(int i : expectedResult){
             System.out.println(i);
         }
     }
     //method is static for ease of testing
-    public static int[] decryptData(int[] price, int discount, int offset, int readLength){
+    public static int[] decryptData(int[] prices, int discount, int offset, int readLength){
         int[] result = {0,0};
-        if(price != null) {
+        if(prices != null) {
             result = new int[readLength];
             int j = 0;
             for (int i = offset; i < offset + readLength; i++) {
-                double priceDouble = price[i];
+                double priceDouble = prices[i];
                 int newPrice = (int) (priceDouble - (priceDouble / 100 * discount));
                 result[j] = (int) newPrice;
                 j++;
