9293ef9886e19249b32b379136214b3ca98120e5
wrxx666 <70403604+wrxx666@users.noreply.github.com>
2021-05-26 01:06:18 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        int[] prices = {5,100,20,66,16}; // Стартовые значения цен из примера
-        int[] expectedResult = {0,0}; // Массив отображаемый в конце, для проверки работоспособности
+        int[] prices = {5,100,20,66,16};
         try {
-            expectedResult = decryptData(prices, 50, 1, 3);
+            int[]expectedResult = decryptData(prices, 50, 1, 3);
+            for(int i : expectedResult){
+                System.out.println(i);
+            }
         }
-        catch (ArrayIndexOutOfBoundsException e){
+        catch (Throwable e){
             e.printStackTrace();
+            System.out.println("Unexpected error while array decrypt cause " + e.getLocalizedMessage());
         }
-        for(int i : expectedResult){
-            System.out.println(i);
-        }
+
     }
-    //method is static for ease of testing
-    public static int[] decryptData(int[] prices, int discount, int offset, int readLength){
+    public static int[] decryptData(int[] price, int discount, int offset, int readLength){
         int[] result = {0,0};
-        if(prices != null) {
+        if(price != null) {
             result = new int[readLength];
-            int j = 0;
-            for (int i = offset; i < offset + readLength; i++) {
-                double priceDouble = prices[i];
+            for (int i = offset, j = 0; i < offset + readLength; i++, j++) {
+                double priceDouble = price[i];
                 int newPrice = (int) (priceDouble - (priceDouble / 100 * discount));
                 result[j] = (int) newPrice;
-                j++;
             }
         }
         return result;
     }
 }
-/*
-public @Nullable int[] decryptData(@NonNull int[] price,
-@IntRange(from = 1) int discount,
-@IntRange(from = 0) int offset,
-@IntRange(from = 1) int readLength) {
- */
